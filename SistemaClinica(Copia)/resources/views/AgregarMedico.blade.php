77a30138d823a354e7728f5c1308faf3504d95db
Gonzalo <gonzaloramirezmelgarejo98@gmail.com>
2019-06-01 17:15:51 -0500

    vistas

---
+++ SistemaClinica(Copia)/resources/views/AgregarMedico.blade.php
@@ -0,0 +1,15 @@
+@extends('plantillas.Admin-plantilla')
+
+@section('title', 'Menu')
+
+@section('contenthead')
+    
+@endsection
+
+@section('contentbody')
+
+
+
+
+
+@endsection
--- SistemaClinica(Copia)/routes/web.php
+++ SistemaClinica(Copia)/routes/web.php
@@ -7,7 +7,7 @@
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| contains the "web" middleware group. Now create something great!  
 |
 */
 
@@ -15,6 +15,30 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::get('adminmenu', function () {
     return view('MenuAdmin');
 });
+
+Route::get('adminhorarios', function () {
+    return view('AdministrarHorarios');
+});
+
+Route::get('adminmedicos', function () {
+    return view('AdministrarHorarios');
+});
+
+Route::get('adminpacientes', function () {
+    return view('AdministrarPaciente');
+});
+
+Route::get('adminmedico', function () {
+    return view('AgregarMedico');
+});
+
+Route::get('adminhorarios/programacionhorarios', function () {
+    return view('programacionhorarios');
+});
+
+
+
