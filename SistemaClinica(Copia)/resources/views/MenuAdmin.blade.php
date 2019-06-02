8f7e1fdbe6161e053c45a1b4ad653f74a9df0727
Gonzalo <gonzaloramirezmelgarejo98@gmail.com>
2019-06-01 19:43:55 -0500

    sidebar-plantilla

--- SistemaClinica(Copia)/resources/views/MenuAdmin.blade.php
+++ SistemaClinica(Copia)/resources/views/MenuAdmin.blade.php
@@ -8,91 +8,73 @@
 
 @section('contentbody')
 
-<br/>
-
 <h1>ADMIN | MENÚ</h1>
 
-<br/>
+<article class="mt-4">
 
-<div class="row">
-        <div class="col-md-2 offset-md-2 ">
-                <a href="">
-                        <div class="card text-black" style="max-width: 12rem;">        
-                                <div class="card-body text-center">
+        <div class="row">
+                <div class="col-md-3 card text-black list-group-item-action">
+                        <a href="#" class="list-group-item-action">
+                                <div class="card-body text-center">        
                                         <img class="" src="{{ asset('iconos/Administrar-pacientes.png') }}" width="100" height="" alt="pacientes logotipo"> 
-                                        <h5 class="card-title text-left">Administrar pacientes</h5>                  
-                                        <p class="card-text text-left">Pacientes Totales:</p>
+                                        <h3 class="card-title">Administrar pacientes</h3>                  
+                                        <p class="card-text text-left">Pacientes Totales:</p>                               
                                 </div>
-                        </div>
-                </a>
-                                
-        </div>
+                        </a>                                        
+                </div>
 
-        <div class="col-md-2 offset-md-1">
-                        <a href="">
-                                <div class="card text-black" style="max-width: 12rem;">        
-                                        <div class="card-body text-center">
-                                                <img class="" src="{{ asset('iconos/Administrar-medicos.png') }}" width="100" height="" alt=" médicos logotipo"> 
-                                                <h5 class="card-title text-left">Administrar médicos</h5>
-                                                <p class="card-text text-left">Médicos Totales:</p>          
-                                        </div>
+                <div class="col-md-3 offset-md-1  card text-black list-group-item-action">
+                        <a href="#" class="list-group-item-action">
+                                <div class="card-body text-center">        
+                                        <img class="" src="{{ asset('iconos/Administrar-medicos.png') }}" width="100" height="" alt=" médicos logotipo"> 
+                                        <h3 class="card-title">Administrar médicos</h3>                  
+                                        <p class="card-text text-left">Médicos Totales:</p>                               
                                 </div>
-                        </a>
-        </div>
+                        </a>                                        
+                </div>
 
-        <div class="col-md-2 offset-md-1">
-                <a href="">
-                        <div class="card text-black" style="max-width: 12rem;">        
-                                <div class="card-body text-center">
-                                <img class="" src="{{ asset('iconos/Administrar-citas.png') }}" width="100" height="" alt=" citas logotipo"> 
-                                <h5 class="card-title text-left">Administrar citas</h5>
-                                <p class="card-text text-left">Citas Totales:</p>
+                <div class="col-md-3 offset-md-1  card text-black list-group-item-action">
+                        <a href="#" class="list-group-item-action">
+                                <div class="card-body text-center">        
+                                        <img class="" src="{{ asset('iconos/Administrar-citas.png') }}" width="100" height="" alt=" citas logotipo">  
+                                        <h3 class="card-title">Administrar citas</h3>                  
+                                        <p class="card-text text-left">Citas Totales:</p>                               
                                 </div>
-                        </div>  
-                </a>
+                        </a>                                        
+                </div>
         </div>
         
-</div>
-
-<br/>
-
-<div class="row">
-        <div class="col-md-2 offset-md-5">
-                <a href="">
-                                <div class="card text-black" style="max-height: 8rem; max-width: 14rem;">        
-                                        <div class="card-body text-center">
+        <div class="row mt-4">
+                <div class="col-md-3 offset-md-4 card text-black list-group-item-action">
+                        <a href="#" class="list-group-item-action">
+                                <div class="card-body text-center">        
                                         <img class="" src="{{ asset('iconos/Especialidades.png') }}" width="60" height="" alt="Especialidades logotipo"> 
-                                        <h5 class="card-title">Especialidades</h5>          
-                                        </div>
+                                        <h3 class="card-title">Especialidades</h3>                  
+                                        <p class="card-text text-left">Pacientes Totales:</p>                               
                                 </div>
-                </a>
+                        </a>                                        
+                </div>        
         </div>
-</div>
-
-<br/>
-
-<div class="row">
-        <div class="col-md-2 offset-md-3">
-                <a href="">
-                                <div class="card text-black" style="max-height: 8rem; max-width: 14rem;">        
-                                        <div class="card-body text-center">
-                                                <img class="" src="{{ asset('iconos/Consultorios.png') }}" width="60" height="" alt="Consultorios logotipo"> 
-                                                <h5 class="card-title">Consultorios</h5>          
-                                        </div>
+        
+        <div class="row mt-2">
+                <div class="col-md-3 offset-md-1 card text-black list-group-item-action">
+                        <a href="#" class="list-group-item-action">
+                                <div class="card-body text-center">        
+                                        <img class="" src="{{ asset('iconos/Consultorios.png') }}" width="60" height="" alt="Consultorios logotipo"> 
+                                        <h3 class="card-title">Consultorios</h3>                             
                                 </div>
-                </a>
-        </div>
-        <div class="col-md-2 offset-md-2">
-                <a href="">
-                                <div class="card text-black" style="max-height: 8rem; max-width: 14rem;">        
-                                        <div class="card-body text-center">
+                        </a>                                        
+                </div>
+                <div class="col-md-3 offset-md-3 card text-black list-group-item-action">
+                                <a href="#" class="list-group-item-action">
+                                        <div class="card-body text-center">        
                                                 <img class="" src="{{ asset('iconos/Medicamentos.png') }}" width="60" height="" alt="Medicamentos logotipo"> 
-                                                <h5 class="card-title">Medicamentos</h5>          
+                                                <h3 class="card-title">Medicamentos</h3>                              
                                         </div>
-                                </div>
-                </a>
-                                
+                                </a>                                        
+                </div> 
         </div>
-</div>
+
+</article>
 
 @endsection
